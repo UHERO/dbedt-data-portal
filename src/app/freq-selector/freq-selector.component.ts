938ad4fd2746ed72343140e65a95f79560f6860c
Victoria Ward <vcward@hawaii.rr.com>
2017-04-17 16:07:31 -1000

    replace multiple select with checkboxes

--- src/app/freq-selector/freq-selector.component.ts
+++ src/app/freq-selector/freq-selector.component.ts
@@ -12,7 +12,6 @@ export class FreqSelectorComponent implements OnInit {
   // If indicator(s) selected, do not display placeholder ('Select an Indicator')
   @Input() indicator: boolean;
   @Input() freqs: Array<Frequency>;
-  @Input() selectedFreqs;
   @Output() selectedFreqList = new EventEmitter();
   private toggleSelected = []
 
@@ -21,23 +20,17 @@ export class FreqSelectorComponent implements OnInit {
   ngOnInit() {
   }
 
-  toggle(e) {
-    let $self = $(e);
-    // If option is selected, deselect and remove from selected list of emitted frequencies
-    if ($self.prop('selected')) {
-      $self.prop('selected', false);
-      let i = this.toggleSelected.indexOf($self.val());
-      if (i > - 1) {
-        this.toggleSelected.splice(i, 1);
+  checked(freq, event) {
+    let index = this.toggleSelected.indexOf(freq);
+    if (event.target.checked) {
+      if (index === -1) {
+        this.toggleSelected.push(freq);
       }
-    // Else, select option and emit frequency value
     } else {
-      $self.prop('selected', true);
-      this.toggleSelected.push($self.val());
+      if (index !== -1) {
+        this.toggleSelected.splice(index, 1);
+      }
     }
-    let options = $('.select-frequency option:selected')
-    this.selectedFreqs = Array.apply(null, options).filter(option => option.selected).map(option => option.value);
-    this.selectedFreqList.emit(this.selectedFreqs);
-    return false;
+    this.selectedFreqList.emit(this.toggleSelected);
   }
 }
--- src/app/geo-selector/geo-selector.component.ts
+++ src/app/geo-selector/geo-selector.component.ts
@@ -13,7 +13,6 @@ export class GeoSelectorComponent implements OnInit {
   // If indicator(s) selected, do not display placeholder ('Select an Indicator')
   @Input() indicator: boolean;
   @Input() regions: Array<Geography>;
-  @Input() selectedGeos;
   @Output() selectedGeoList = new EventEmitter();
   private toggleSelected = [];
 
@@ -22,23 +21,17 @@ export class GeoSelectorComponent implements OnInit {
   ngOnInit() {
   }
 
-  toggle(e) {
-    let $self = $(e);
-    // If option is selected, deselect and remove from selected list of emitted regions
-    if ($self.prop('selected')) {
-      $self.prop('selected', false);
-      let i = this.toggleSelected.indexOf($self.val());
-      if (i > - 1) {
-        this.toggleSelected.splice(i, 1);
+  checked(region, event) {
+    let index = this.toggleSelected.indexOf(region);
+    if (event.target.checked) {
+      if (index === -1) {
+        this.toggleSelected.push(region);
       }
-    // Else, select option and emit region value
     } else {
-      $self.prop('selected', true);
-      this.toggleSelected.push($self.val());
+      if (index !== -1) {
+        this.toggleSelected.splice(index, 1);
+      }
     }
-    let options = $('.select-region option:selected')
-    this.selectedGeos = Array.apply(null, options).filter(option => option.selected).map(option => option.value);
-    this.selectedGeoList.emit(this.selectedGeos);
-    return false;
+    this.selectedGeoList.emit(this.toggleSelected);
   }
 }
