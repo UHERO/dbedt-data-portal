60d15b18c2d6342ee9009bc7f5dc1f8a1507a5ab
Victoria Ward <vward@hawaii.edu>
2023-07-26 18:25:03 -0400

    edit style of category menu to match old style

--- src/app/category-sidebar/category-sidebar.component.ts
+++ src/app/category-sidebar/category-sidebar.component.ts
@@ -5,6 +5,7 @@ import { TreeNode } from 'primeng/api';
 import { TreeNodeSelectEvent } from 'primeng/tree';
 import { Subscription } from 'rxjs';
 import { Category } from 'app/category';
+import { faCaretRight, faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 interface CustomTreeNode extends TreeNode {
   parentId: number;
@@ -32,12 +33,15 @@ export class CategorySidebarComponent implements OnInit, OnDestroy {
   // Emit ids of selected categories to app.component
   @Output() selectedCatIds = new EventEmitter();
   selectedNodes: CustomTreeNode[];
+  faCaretRight = faCaretRight;
+  faCaretDown = faCaretDown;
 
   constructor(private _apiService: ApiService) { }
 
   ngOnInit() {
     this.categorySubscription = this._apiService.fetchCategories().subscribe((data) => {
       this.categories = data;
+      console.log(data)
     });
   }
 
